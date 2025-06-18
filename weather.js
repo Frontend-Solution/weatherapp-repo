f7588b15d3eaf702e54abbbc127be5cc534f3f5c
Frontend-Solution <shumailakhan18d@gmail.com>
2025-06-18 22:52:09 +0500

    weather app

---
+++ weather.js
@@ -0,0 +1,69 @@
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
+const location_not_found = document.querySelector('.location-not-found');
+const weather_body = document.querySelector('.weather-body');
+
+// Weather API Function
+
+async function checkWeather(city) {
+  const api_key = "5dedb5ba05ce5572bc905e95219da1a9";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+  const weather_data = await fetch(`${url}`).then(response => response.json());
+
+  // 404 error
+
+  if (weather_data.cod === `404`) {
+    location_not_found.style.display = "flex";
+    weather_body.style.display = "none";
+    return;
+  }
+  location_not_found.style.display = "none";
+  weather_body.style.display = "flex";
+
+
+
+  temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+
+  description.innerHTML = `${weather_data.weather[0].description}`;
+
+  humidity.innerHTML = `${weather_data.main.humidity}%`;
+
+  wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+
+
+
+
+  switch (weather_data.weather[0].main) {
+    case 'Clouds':
+      weather_img.src = "/weather/weatherimg/sunny_cloud-removebg-preview.png"; break;
+
+    case 'Sunny':
+      weather_img.src = "/weather/weatherimg/sunny-removebg-preview.png"; break;
+
+    case 'Rain':
+      weather_img.src = "/weather/weatherimg/cloud_rain-removebg-preview.png"; break;
+
+    case 'Snow':
+      weather_img.src = "/weather/weatherimg/ssnow-removebg-preview.png"; break;
+
+    case 'Mist':
+      weather_img.src = "/weather/weatherimg/drop-removebg-preview.png"; break;
+
+    case 'Clear':
+      weather_img.src = "/weather/weatherimg/sunny-removebg-preview.png"; break;
+
+  }
+}
+
+
+searchBtn.addEventListener('click', () => {
+  checkWeather(inputBox.value);
+});
+
+
